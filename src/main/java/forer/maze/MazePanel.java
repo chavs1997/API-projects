7463c1b3ef2c4a2309175bda8f92846fec4b4856
chavs1997 <vforer@student.touro.edu>
2018-10-18 17:56:31 -0400

    mazeGUI works; still haven't added a character to wwalk through

--- src/main/java/forer/maze/MazePanel.java
+++ src/main/java/forer/maze/MazePanel.java
@@ -6,33 +6,41 @@ import java.awt.*;
 public class MazePanel extends JComponent {
 
     private Maze maze;
-    private int size = 5;
+    private int size;
 
-    public MazePanel() {
-        maze = new Maze(size);
+    public MazePanel(int length) {
+        size = length;
+        maze = new Maze(length);
     }
 
     public void paintComponent(Graphics g) {
 
         Graphics2D g2 = (Graphics2D) g;
-        BasicStroke stroke = new BasicStroke(50);
+        BasicStroke stroke = new BasicStroke(15);
         g2.setStroke(stroke);
         g2.setColor(Color.BLUE);
+        super.paintComponent(g);
+        int height = this.getHeight()/size;
 
-        for (int i = 0; i < maze.size; i++) {
-            for (int j = 0; j < maze.size; j++) {
-                if (maze.getCell(i, j).isEWall()) {
-                    g2.drawLine(i, j + 1, i + 1, j + 1);
+        for (int column = 0; column < size; column++) {
+            for (int row = 0; row < size; row++) {
+                Cell current = maze.getCell(column, row);
+                if(current.isWWall()) {
+                    g.drawLine((row+1)*height, column*height, (row+1)*height, column*height + height);
                 }
-                if (maze.getCell(i, j).isNWall()) {
-                    g2.drawLine(i, j, i, j + 1);
+
+                if(current.isNWall()){
+                    g.drawLine((row+1)*height, column*height,(row+1)*height + height, column*height);
                 }
-                if (maze.getCell(i, j).isSWall()) {
-                    g2.drawLine(i + 1, j, i + 1, j + 1);
+
+                if(current.isEWall()) {
+                    g.drawLine((row+1)*height + height, column*height, (row+1)*height + height, column*height + height);
                 }
-                if (maze.getCell(i, j).isWWall()) {
-                    g2.drawLine(i, j, i + 1, j);
+
+                if(current.isSWall()) {
+                    g.drawLine((row+1)*height, column*height + height, (row+1)*height + height, column*height +height);
                 }
+
             }
         }
     }
--- src/main/java/forer/maze/MazeView.java
+++ src/main/java/forer/maze/MazeView.java
@@ -3,59 +3,36 @@ package forer.maze;
 import javax.swing.*;
 
 public class MazeView extends JFrame {
-    public MazeView() {
+    public MazeView(int length) {
         setTitle("Maze");
-        setSize(800, 800);
+        setSize(1000,1000);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        MazePanel maze = new MazePanel();
+        MazePanel maze = new MazePanel(length);
         add(maze);
     }
 
     public static void main(String[] args) {
 
-//        JDialog.setDefaultLookAndFeelDecorated(true);
-//        Object[] selectionValues = {"5 x 5", "10 x 10", "15 x 15", "20 x 20", "50 x 50"};
-//        String initialSelection = "5 x 5";
-//        Object selection = JOptionPane.showInputDialog(null, "Choose a mazeGrid size:",
-//                "Maze Display", JOptionPane.QUESTION_MESSAGE, null, selectionValues, initialSelection);
-//        String selected = selection.toString();
-//        int length;
-//        if (selected.equals("5 x 5")) {
-//            length = 5;
-//        } else if (selected.equals("10 x 10")) {
-//            length = 10;
-//        } else if (selected.equals("15 x 15")) {
-//            length = 15;
-//        } else if (selected.equals("20 x 20")) {
-//            length = 20;
-//        } else if (selected.equals("50 x 50")) {
-//            length = 50;
-//        } else {
-//            length = 10;
-//        }
+        JDialog.setDefaultLookAndFeelDecorated(true);
+        Object[] selectionValues = {"5 x 5", "10 x 10", "15 x 15", "20 x 20"};
+        String initialSelection = "5 x 5";
+        Object selection = JOptionPane.showInputDialog(null, "Choose a mazeGrid size:",
+                "Maze Display", JOptionPane.QUESTION_MESSAGE, null, selectionValues, initialSelection);
+        String selected = selection.toString();
+        int length;
+        if (selected.equals("5 x 5")) {
+            length = 5;
+        } else if (selected.equals("10 x 10")) {
+            length = 10;
+        } else if (selected.equals("15 x 15")) {
+            length = 15;
+        } else if (selected.equals("20 x 20")) {
+            length = 20;
+        }  else {
+            length = 10;
+        }
+            new MazeView(length).setVisible(true);
 
-       new MazeView().setVisible(true);
-
-//        Maze maze = new Maze(7);
-//        for(int i = 0;i < 7; i++){
-//            for(int j = 0; j < 7; j++){
-//                if (maze.getCell(i, j).isWWall()) {
-//                    System.out.print("|");
-//                }
-//                if (maze.getCell(i, j).isNWall()) {
-//                    System.out.print(" ^ ");
-//                }
-//                if (maze.getCell(i, j).isEWall()) {
-//                    System.out.print("|");
-//                }
-//
-//                if (maze.getCell(i, j).isSWall()) {
-//                    System.out.print("_");
-//                }
-//
-////            }
-//            System.out.println();
-//        }
     }
 }
