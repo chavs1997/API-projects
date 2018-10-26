5d2e80f581f7409117856313a5c663729f6a3982
chavs1997 <vforer@student.touro.edu>
2018-10-25 20:03:07 -0400

    mouseevent added

--- src/main/java/forer/paint/Canvas.java
+++ src/main/java/forer/paint/Canvas.java
@@ -1,5 +1,8 @@
 package forer.paint;
 
+import javafx.scene.shape.Circle;
+import sun.java2d.loops.DrawRect;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
@@ -7,13 +10,15 @@ import java.awt.event.MouseMotionListener;
 
 
 public class Canvas extends JComponent{
+    MouseEvent event;
     public Canvas(){
+
             setBackground(Color.WHITE);
             addMouseMotionListener(new MouseMotionListener() {
                 @Override
                 public void mouseDragged(MouseEvent e) {
-                    System.out.println(e.getX() + ", " + e.getY());
-                }
+                        event = e;
+                                    }
 
                 @Override
                 public void mouseMoved(MouseEvent e) {
@@ -22,5 +27,14 @@ public class Canvas extends JComponent{
             });
 
     }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.drawOval(event.getX(), event.getY(), 1, 1);
+
+    }
+
 
 }
