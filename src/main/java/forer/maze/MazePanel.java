52f2ebf1a2b8c54820adfc60780dc4a101eaf5e7
chavs1997 <vforer@student.touro.edu>
2018-11-25 23:54:55 -0500

    maze complete

--- src/main/java/forer/maze/MazePanel.java
+++ src/main/java/forer/maze/MazePanel.java
@@ -1,16 +1,40 @@
 package forer.maze;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class MazePanel extends JComponent {
 
     private Maze maze;
-    private int size;
+    private int length;
+    Image girlImage;
+    private int height;
+    private int squareHeight;
+    private Girl girl = new Girl(0,0);
+    private BufferedImage read;
 
     public MazePanel(int length) {
-        size = length;
+        this.length = length;
         maze = new Maze(length);
+        try {
+            read = ImageIO.read(new File("running-girl.jpg"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public Girl getGirl() {
+        return girl;
     }
 
     public void paintComponent(Graphics g) {
@@ -19,29 +43,61 @@ public class MazePanel extends JComponent {
         BasicStroke stroke = new BasicStroke(15);
         g2.setStroke(stroke);
         g2.setColor(Color.BLUE);
+        height = this.getHeight();
+        squareHeight = height / length;
         super.paintComponent(g);
-        int height = this.getHeight() / size;
-
-        for (int column = 0; column < size; column++) {
-            for (int row = 0; row < size; row++) {
-                Cell current = maze.getCell(column, row);
+        for (int row = 0; row < length; row++) {
+            for (int column = 0; column < length; column++) {
+                Cell current = maze.getCell(row, column);
                 if (current.isWWall()) {
-                    g.drawLine((row + 1) * height, column * height, (row + 1) * height, column * height + height);
+                    g.drawLine(column  * squareHeight, row * squareHeight, column * squareHeight, (row + 1) * squareHeight);
                 }
 
                 if (current.isNWall()) {
-                    g.drawLine((row + 1) * height, column * height, (row + 1) * height + height, column * height);
+                    g.drawLine(column * squareHeight, row * squareHeight, (column+1)  * squareHeight, row  * squareHeight);
                 }
 
                 if (current.isEWall()) {
-                    g.drawLine((row + 1) * height + height, column * height, (row + 1) * height + height, column * height + height);
+                    g.drawLine((column + 1) * squareHeight, row * squareHeight, (column + 1) * squareHeight, (row + 1) * squareHeight);
                 }
 
                 if (current.isSWall()) {
-                    g.drawLine((row + 1) * height, column * height + height, (row + 1) * height + height, column * height + height);
+                    g.drawLine(column * squareHeight, (row + 1) * squareHeight, (column + 1) * squareHeight, (row + 1) * squareHeight);
                 }
 
             }
+        }
+
+        paintGirl(g);
+    }
+
+    private void paintGirl(Graphics g) {
+        girlImage = read.getScaledInstance(squareHeight-20, squareHeight-20, Image.SCALE_DEFAULT);
+        g.drawImage(girlImage,(girl.getColumn()*squareHeight) + 10,
+                (girl.getRow()*squareHeight) + 10 , null);
+    }
+
+    public void directionLeft() {
+        if(girl.getColumn() != 0 && !maze.getCell(girl.getRow(), girl.getColumn()).isWWall()){
+            girl.setColumn(girl.getColumn() - 1);
+        }
+    }
+
+    public void directionRight() {
+        if(girl.getColumn() != length && !maze.getCell(girl.getRow(), girl.getColumn()).isEWall()){
+            girl.setColumn(girl.getColumn() + 1);
+        }
+    }
+
+    public void directionUp() {
+        if(girl.getRow() != 0 && !maze.getCell(girl.getRow(), girl.getColumn()).isNWall()){
+            girl.setRow(girl.getRow() - 1);
+        }
+    }
+
+    public void directionDown() {
+        if(girl.getRow() != length && !maze.getCell(girl.getRow(), girl.getColumn()).isSWall()){
+            girl.setRow(girl.getRow() + 1);
         }
     }
 }
--- src/main/java/forer/maze/MazeView.java
+++ src/main/java/forer/maze/MazeView.java
@@ -1,16 +1,55 @@
 package forer.maze;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class MazeView extends JFrame {
+
+    MazePanel maze;
     public MazeView(int length) {
         setTitle("Maze");
         setSize(1000, 1000);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-        MazePanel maze = new MazePanel(length);
+        maze = new MazePanel(length);
+        maze.setBackground(Color.WHITE);
         add(maze);
+        addKeyListener(new MyKeyAdapter());
     }
+
+    private class MyKeyAdapter extends KeyAdapter {
+
+        @Override
+        public void keyPressed(KeyEvent keyEvent) {
+
+            if (keyEvent.getKeyCode() == KeyEvent.VK_LEFT) {
+                maze.directionLeft();
+                repaint();
+                checkIfDone();
+            } else if (keyEvent.getKeyCode() == KeyEvent.VK_RIGHT) {
+                maze.directionRight();
+                repaint();
+                checkIfDone();
+            } else if (keyEvent.getKeyCode() == KeyEvent.VK_UP) {
+                maze.directionUp();
+                repaint();
+                checkIfDone();
+            } else if (keyEvent.getKeyCode() == KeyEvent.VK_DOWN) {
+                maze.directionDown();
+                repaint();
+                checkIfDone();
+            }
+        }
+    }
+
+    private void checkIfDone() {
+        if(maze.getGirl().getRow() == maze.getLength() - 1 && maze.getGirl().getColumn() == maze.getLength() - 1){
+            System.out.println("You won!!!");
+            System.exit(0);
+        }
+    }
+
 
     public static void main(String[] args) {
 
